be1abc2b1b6a60a0261d591e212ac30ce81c25ac
Abhinav <AbhinavSunil@hotmail.com>
2026-02-10 00:31:50 +0530

    Feat : added statuscodes on it restful api practices

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,3 +1,5 @@
+const { StatusCodes } = require('http-status-codes');
+
 // Ping controller function - returns status (working)
 function pingProblemChecker(req, res) {
   return res.json({ message: 'Ping controller is up' });
@@ -5,35 +7,35 @@ function pingProblemChecker(req, res) {
 
 // Add a new problem (working)
 function addProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: 'Not implemented',
   });
 }
 
 // Get a specific problem by ID (working)
 function getProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: 'Not implemented',
   });
 }
 
 // Get all problems (working)
 function getProblems(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: 'Not implemented',
   });
 }
 
 // Delete a problem by ID (working)
 function deleteProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: 'Not implemented',
   });
 }
 
 // Update a problem by ID (working)
 function updateProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: 'Not implemented',
   });
 }
